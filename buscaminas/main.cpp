23766b4758c7eaf616ccf5afec362b5b9a1a5590
zareekgalvan <zareek99@gmail.com>
2015-01-20 09:01:57 -0600

    Initial Commit

---
+++ buscaminas/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  buscaminas
+//
+//  Created by Zareek Galvan on 1/20/15.
+//  Copyright (c) 2015 Zareek Galvan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
